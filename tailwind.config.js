9e37b56560790c7f40c676fbd2c34583af1e429a
niezk <mhafidzarrahman4@gmail.com>
2022-12-26 21:27:33 +0700

    Fixing some bug and add someting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
+    fontFamily: {
+      Prosto: [
+        "Prosto One, cursive",
+      ],
+      RobotoSlab: [
+        "Roboto Slab, serif"
+      ],
+      Caramel: [
+        "Caramel, cursive"
+      ],
+    },
     extend: {},
   },
   plugins: [],
